fb9479d9c38208688f21b35a47f50f0c5acfb1df
Muhammad Husnain <mlhlk1212@gmail.com>
2024-09-25 23:04:05 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Abdul Baist Khan",
+  title: "Abdul Basit Khan",
   description: "Abdul Basit khan Project Manager",
 };
 
